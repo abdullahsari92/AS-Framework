febaa07ef2be8005f0a443767ed94b2e044dc21f
abdullahsari92 <abdullahsari92@gmail.com>
2022-12-14 14:46:50 +0300

    Abdullah Sarı tarafından yeni ald yapı projesi dotnet core ile yapılmaya başlandı.

---
+++ AS.Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace DPU.Entities.BaseEntities
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Veri tabanı nesneleri için temel nesne
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Birincil anahtar
+        /// </summary>
+        public int Id { get; set; }
+       
+        /// <summary>
+        /// Oluşturulma zamanı
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+
+        public int CreatedById { get; set; }
+        /// <summary>
+        /// Oluşturan kullanıcı
+        /// </summary>
+        public virtual User CreatedBy { get; set; }
+
+
+
+
+    }
+
+}
---
+++ AS.Entities/Dtos/User.cs
@@ -0,0 +1,11 @@
+﻿using DPU.Entities.BaseEntities;
+
+namespace DPU.Entities
+{
+    public class User: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
